d14cf934b8c978b64072df62e48fcc2389c7adf8
Roel Hogervorst <hogervorst.rm@gmail.com>
2016-08-14 11:42:59 +0200

    trying several versions of creation

--- datasetcreation.R
+++ datasetcreation.R
@@ -49,11 +49,42 @@ werepeople$first_name <- ifelse(werepeople$sex == "female",
                                 sample(female_names, N_people),
                                 sample(male_names, N_people))
 
-#werepeople$type <- ifelse(werepeople$moonphase == "Full", "werewolf", NA)
+werepeople$type <- ifelse(werepeople$moonphase == "Full", "werewolf", NA)
 
 werepeople$value1 <- werepeople$BMI *as.numeric(werepeople$weekday) - as.numeric(werepeople$moonphase)
 werepeople$value2 <- werepeople$BFI_N *werepeople$BFI_O**werepeople$value3
 werepeople$value3 <- rnorm(N_people)
 werepeople$value4 <- werepeople$value1+werepeople$value2+werepeople$value3
-werepeople$type <- ifelse(werepeople$value4 >=100, "normal",
-                          ifelse(werepeople$value3 >0.3, "werewolf", "wererabbit"))
+werepeople$type[werepeople$value3 <0] <- as.factor(ifelse(werepeople$value4 >=100, "normal",
+                           ifelse(werepeople$value3 >0.3, "werewolf", "wererabbit")))
+werepeople$typewerepeople$type[werepeople$value3 >=0] <- sample(c("normal", "werewolf", "wererabbit"),
+                                                                size = length(werepeople$typewerepeople$type[werepeople$value3 >=0]),
+                                                                replace = TRUE,prob = c(2,1,1))
+# if favorite food is Bacon or Steak and moon is full during birth werewolf
+# if favorite food is blueberries, apples, banana, sweetcorn and age <30 wererabbit
+#################creation of probabilities.
+#vglm(formec.domainTOTAALn, family=multinomial(refLevel=2), data=datac1domecTOTAAL)
+#formulaprediction <- type~moonphase+weekday+ age + scratch + allergies + eye_color +sex +blood_type
+formulaprediction <- type~moonphase+weekday+ age + scratch + eye_color +sex +blood_type+BFI_N
+
+library(VGAM)
+predictie <- vglm(formula = formulaprediction,
+     family =multinomial(refLevel=1),
+     data = werepeople)
+#summary(predictie)
+values <- predict(predictie, werepeople)
+
+noemer <- 1+ exp(values[,1])+ exp(values[,2])
+normal <- exp(values[,1])/noemer
+wererabbit <- exp(values[,2])/noemer
+werewolf <- 1/noemer
+wolfies <- cbind(normal,wererabbit, werewolf)
+
+
+#wolfies$choose <- max.col(wolfies[,1:3])
+#rowSums(wolfies)
+# cbind(werepeople,wolfies) %>% select(type,normal, wererabbit) %>% 
+#     View
+
+werepeople_dataset <- cbind(werepeople,wolfies) %>% select(-type, -value4, value1,value2, value3)
+saveRDS(werepeople_dataset, "data/werepeople.RDS")
